03068b4b4069b8df57da765896af36803a98fffd
ColinKieu <colin.kieu@ocsbstudent.ca>
2021-03-24 16:27:32 +0000

    linter

--- script.js
+++ script.js
@@ -9,15 +9,12 @@ function verify () {
     document.getElementById('answer').innerHTML = 'You Can see an R movie alone.'
 }
 
-  else if (age >= 13) {
+  else if (age >= 13) 
+  {
     document.getElementById('answer').innerHTML = 'You can see a PG-13 movie alone.'
-}
-
-  else if (age >= 5) {
+  } else if (age >= 5) {
     document.getElementById('answer').innerHTML = 'You can see a G or a PG movie alone.'
-}
-
-  else {
+  } else {
     document.getElementById('answer').innerHTML = 'Uh, you are too young for most things.'
-}
+  }
 }
